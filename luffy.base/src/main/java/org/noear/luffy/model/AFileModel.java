981d24b967638dc6d5b44dc03afd637dbf85233f
noear <noear@live.cn>
2021-07-05 14:10:16 +0800

    1.1.4

--- luffy.base/src/main/java/org/noear/luffy/model/AFileModel.java
+++ luffy.base/src/main/java/org/noear/luffy/model/AFileModel.java
@@ -4,25 +4,39 @@ import java.util.Date;
 
 public class AFileModel {
     private static ThreadLocal<AFileModel> _current = new ThreadLocal();
+
+    /**
+     * 获取当前文件
+     * */
     public static AFileModel current(){
         return _current.get();
     }
 
+    /**
+     * 设置当前文件
+     * */
     public static void currentSet(AFileModel file){
         _current.set(file);
     }
 
+    /**
+     * 移除当前文件
+     * */
     public static void currentRemove(){
         _current.remove();
     }
 
 
-    /**  */
+    /** 文件id */
     public int file_id;
+    /** 文件类型 */
     public int file_type;
 
+    /** 分组标签 */
     public String tag;
+    /** 属性标记 */
     public String label;
+    /** 备注 */
     public String note;
 
     /** 路径 */
@@ -42,14 +56,22 @@ public class AFileModel {
     /** 内容 */
     public String content;
 
+    /** 计划状态 */
     public int plan_state;
+    /** 计划开始时间 */
     public Date plan_begin_time;
+    /** 计划最近执行时间 */
     public Date plan_last_time;
+    /** 计划最近执行时长 */
     public long  plan_last_timespan;
+    /** 计划间隔时间 */
     public String plan_interval;
+    /** 计划最多执行次数 */
     public int plan_max;
+    /** 计划执行次数 */
     public int plan_count;
 
+    /** 使用白名单 */
     public String use_whitelist;
 
     /** 创建时间 */
