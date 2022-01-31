9cb3976c67d09826145e6a42b46bd5c8cd28b026
noear <noear@live.cn>
2022-01-31 11:45:51 +0800

    1.1.13

--- luffy-jtl/src/main/java/org/noear/localjt/LocalJtApp.java
+++ luffy-jtl/src/main/java/org/noear/localjt/LocalJtApp.java
@@ -85,9 +85,10 @@ public class LocalJtApp {
                 WebShell.start(args);
             }).start();
         } else {
-            //尝试浏览器打开
-            if (java.awt.Desktop.isDesktopSupported()) {
-                try {
+            //尝试用本地浏览器打开
+            try {
+                if (java.awt.Desktop.isDesktopSupported()) {
+
                     // 创建一个URI实例
                     java.net.URI uri = java.net.URI.create(home);
                     // 获取当前系统桌面扩展
@@ -97,10 +98,9 @@ public class LocalJtApp {
                         // 获取系统默认浏览器打开链接
                         dp.browse(uri);
                     }
-
-                } catch (Throwable e) {
-                    e.printStackTrace();
                 }
+            } catch (Throwable e) {
+                //跳过异常
             }
         }
     }
