96fabe58d4a92b82abfbaf88c267b6c416e3c546
noear <noear@live.cn>
2023-03-31 07:50:20 +0800

    增强 md 渲染功能

--- custom/luffy.cap.extend.flexmark/src/main/java/org/noear/luffy/cap/extend/flexmark/eMark.java
+++ custom/luffy.cap.extend.flexmark/src/main/java/org/noear/luffy/cap/extend/flexmark/eMark.java
@@ -19,8 +19,8 @@ public class eMark {
         String html = MarkdownUtils.markdown2Html(markdown);
 
         String html2 = html
-                .replace("<li>[ ]", "<li class='task-list-item'><input type=\"checkbox\" disabled=\"\">")
-                .replace("<li>[x]","<li class='task-list-item'><input type=\"checkbox\" disabled=\"\" checked=\"\">")
+                //.replace("<li>[ ]", "<li class='task-list-item'><input type=\"checkbox\" disabled=\"\">")
+                //.replace("<li>[x]","<li class='task-list-item'><input type=\"checkbox\" disabled=\"\" checked=\"\">")
                 .replace("\r\n\r\n", "<br/>");
 
         return  html2;
--- custom/luffy.cap.extend.flexmark/src/main/java/org/noear/luffy/cap/extend/flexmark/utils/MarkdownUtils.java
+++ custom/luffy.cap.extend.flexmark/src/main/java/org/noear/luffy/cap/extend/flexmark/utils/MarkdownUtils.java
@@ -1,26 +1,38 @@
 package org.noear.luffy.cap.extend.flexmark.utils;
 
-import com.vladsch.flexmark.Extension;
-import com.vladsch.flexmark.ast.Document;
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
+import com.vladsch.flexmark.ext.definition.DefinitionExtension;
+import com.vladsch.flexmark.ext.escaped.character.EscapedCharacterExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
 import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.parser.ParserEmulationProfile;
-import com.vladsch.flexmark.util.options.MutableDataSet;
+import com.vladsch.flexmark.util.ast.Document;
+import com.vladsch.flexmark.util.builder.Extension;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 import org.noear.solon.Utils;
 
 import java.util.Arrays;
 
 public class MarkdownUtils {
 
-    public static String markdown2Html(String markdown)  {
-        if(Utils.isEmpty(markdown)){
+    public static String markdown2Html(String markdown) {
+        if (Utils.isEmpty(markdown)) {
             return markdown;
         }
 
         MutableDataSet options = new MutableDataSet();
         options.setFrom(ParserEmulationProfile.MARKDOWN);
-        options.set(Parser.EXTENSIONS, Arrays.asList(new Extension[] { TablesExtension.create()}));
+        options.set(Parser.SPACE_IN_LINK_URLS, true);
+        options.set(Parser.EXTENSIONS, Arrays.asList(new Extension[]{
+                TablesExtension.create(),
+                TaskListExtension.create(),
+                DefinitionExtension.create(),
+                AutolinkExtension.create(),
+                StrikethroughExtension.create(),
+                EscapedCharacterExtension.create()}));
 
 
         Parser parser = Parser.builder(options).build();
